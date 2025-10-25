c6aaabb4714dff73a2ca2ea2f416731b3a2de9fb
sanojkrishnan <sanojkrishnan12@gmail.com>
2025-10-25 19:22:35 +0530

    date 25/10/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./App.css"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Settings from "./Pages/Settings";
+import Usage from "./Pages/Usage";
+import Users from "./Pages/Users";
+import NotFound from "./Pages/NotFound";
+import Header from "./Components/Header";
+
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter >  {/* wrapper component for routing. this helps with reading ULs, to store history, and also with navigation */}
+    <div>
+      <Header /> {/**This component renders the navigation links */}
+    </div>
+    <div className="page">
+      <Routes>  {/**This component is responsible for rendering the appropriate component based on the current URL */}
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/usage" element={<Usage />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <NavLink to="/">
+        <span>Home</span>
+      </NavLink>     {/**Link component is used to navigate between routes*/}
+      <NavLink to="settings">
+        <span>Settings</span>
+      </NavLink>
+      <NavLink to="usage">
+        <span>Usage</span>
+      </NavLink>
+      <NavLink to="users">
+        <span>Users</span>
+      </NavLink>
+    </div>
+  );
+}
+
+export default Header;
