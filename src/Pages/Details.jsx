32b8ead095d22f869a14255709fc044139fe9717
sanojkrishnan <sanojkrishnan12@gmail.com>
2025-10-27 20:50:22 +0530

    date 27/10/2025

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,20 +1,34 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import "./Details.css";
+import axios from "axios";
 
 function Details() {
-  const obj = useParams();
-  const [userData, setUserData] = useState(null);
-  console.log(obj);
-  const userId = obj.id;
+  const obj = useParams(); //useParams hook is used to access the dynamic parameters from the URL. here we are accessing the "id" parameter
+  const urlId = obj.id;
+  console.log("userId", urlId); //id from the url
+  const location = useLocation(); //useLocation hook is used to access the current location object, which contains information about the current URL
+  const state = location.state || {}; //state property of the location object is used to access the state data that was passed during navigation to this route
+  const navigate = useNavigate(); //useNavigate hook is used to programmatically navigate to different routes in the application
+  const [userData, setUserData] = useState(state.user); //initializing userData state with the user data from the location state
+
   useEffect(() => {
-    axios("/Users.json").then((response) => {
-      console.log("resp", response.data);
-      const user = response.data.find((data) => data.id === parseInt(userId));
-      setUserData(user);
-    });
-  }, [userId]);
+    //fetching user data if not available in state
+    if (!userData) {
+      //only fetch if userData is not already available
+      axios("/Users.json").then((response) => {
+        //fetching the Users.json file
+        console.log("resp", response.data); //logging the response data
+        const user = response.data.find((data) => data.id === parseInt(urlId)); //finding the user with the matching id from the response data
+        setUserData(user); //setting the userData state with the found user data
+      });
+    }
+  }, [urlId, userData]); //dependencies for the useEffect hook
+
+  if (!userData) {
+    return null; //or a loading indicator can be returned here
+  }
+
   return (
     <div className="details">
       <h1 className="title">
@@ -28,7 +42,16 @@ function Details() {
       <div className="body">
         {userData ? userData.address : " "} <br />
         {userData ? userData.class : " "} <br />
-        {userData ? userData.phone : " "} 
+        {userData ? userData.phone : " "}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            navigate(-1); //-1 means go back to the previous page
+          }}
+        >
+          Back
+        </button>
       </div>
     </div>
   );
--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -1,31 +1,78 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./Users.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 
 function Users() {
   const [data, setData] = useState([]);
+  const [params, setParams] = useSearchParams({}); //useSearchParams hook is used to read and modify the query string in the URL . its a react router dom hook
+  const cls = params.get("class"); //get method is used to get the value of a specific query parameter from the URL. here we are getting the value of the "class" parameter
+
+  const navigate = useNavigate(); //useNavigate hook is used to programmatically navigate to different routes in the application 
+
+  const handleClick = (id) => {
+    navigate(`/details/${id}`, {
+      //replace: true, ////////remember this option, the issue that showing in browser back button is because of this option
+      state: {
+        user: data.find((item) => item.id === id),
+      },
+    }); //navigating to the details page of the user with the given id //replace:true means the current entry in the history stack will be replaced with the new one. //state:{} is used to pass additional state data to the target route
+  }
 
   useEffect(() => {
     axios("Users.json").then((response) => {
       setData(response.data);
     });
-  });
+  },[]);
+
+  const handleSelectChange =(e) => {
+    if (e.target.value === "All") {
+      setParams({}); //if "All" is selected,we clear the query parameters from the URL
+      
+    }
+    else{setParams({class: e.target.value});} //otherwise we set the "class" query parameter to the selected value
+  }
   return (
     <div className="page users">
-      {data.map((item) => {
-        return (
-          <div className="users-item" key={item.id}>
-            <div className="title">
-              <Link to={`/details/${item.id}`}>{item.name}</Link>
+      <div className="options">
+        <div>Select Class:</div>
+        <div>
+          <select onChange={handleSelectChange} value={cls || "All"}>
+            <option>All</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+          </select>
+        </div>
+      </div>
+      {data
+        .filter((item) => {
+          if (!cls) {
+            return true;
+          }
+          return item.class === cls;
+        })
+        .map((item) => {
+          return (
+            <div className="users-item" key={item.id} onClick={() => handleClick(item.id)}>
+              <div className="title">
+                {item.name}
+              </div>
+              <div className="description">
+                {item.address}
+                <br />
+                {item.class}
+              </div>
             </div>
-            <div className="description">
-              {item.address}<br />
-              {item.class}
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
