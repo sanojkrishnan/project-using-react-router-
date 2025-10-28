feb2a4afa2128488158b2c1011f7249ad253f23c
sanojkrishnan <sanojkrishnan12@gmail.com>
2025-10-28 23:12:49 +0530

    date 28/10/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css"
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Settings from "./Pages/Settings";
 import Usage from "./Pages/Usage";
